527e11a0458ef59cba279fbf8434755fbf09c7ff
Domenico Donnici <lildododonnici05@gmail.com>
2024-01-22 22:05:56 +0100

    Css aggiornato

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import Logo from "../img/logo.png";
 
 const Footer = () => {
   return (
     <footer>
-      <img src={Logo} alt="" />
       <span>
         Fatto con ♥️ e <b>React.js</b>.
       </span>
